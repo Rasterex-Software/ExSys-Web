d118fa491e66dcb699d0a9d152bba5c6001d99a5
Pavlo Antosiak <pavlo.antosiak@gmail.com>
2023-12-14 19:34:03 +0200

    Update prod env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    webViewerUrl: "http://ec2-3-75-211-117.eu-central-1.compute.amazonaws.com:81",
+    rxWebViewerUrl: "http://ec2-3-75-211-117.eu-central-1.compute.amazonaws.com:81",
     rxServerUrl: "http://ec2-3-75-211-117.eu-central-1.compute.amazonaws.com",
     webTitle: "Your system",
     apiBaseUrl: "http://localhost:3000"
